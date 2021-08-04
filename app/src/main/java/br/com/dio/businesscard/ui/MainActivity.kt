a8b8fb31276871f048c93f205355bcc2c7ea7037
Vitor Ferreira <vitorfer238@gmail.com>
2021-08-03 21:45:33 -0300

    creating AddBusinessCardActivity.kt layout

--- app/src/main/java/br/com/dio/businesscard/ui/MainActivity.kt
+++ app/src/main/java/br/com/dio/businesscard/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.dio.businesscard.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.dio.businesscard.databinding.ActivityMainBinding
@@ -10,5 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.fab.setOnClickListener {
+            val intent = Intent(this, AddBusinessCardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
