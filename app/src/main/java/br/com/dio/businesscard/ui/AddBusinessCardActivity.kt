c65ebc15fac6aaa47cde701481103b95a437bd7a
Vitor Ferreira <vitorfer238@gmail.com>
2021-08-06 15:39:38 -0300

    saving business cards

--- app/src/main/java/br/com/dio/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/br/com/dio/businesscard/ui/AddBusinessCardActivity.kt
@@ -1,11 +1,20 @@
 package br.com.dio.businesscard.ui
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import br.com.dio.businesscard.App
+import br.com.dio.businesscard.R
+import br.com.dio.businesscard.data.BusinessCard
 import br.com.dio.businesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -17,7 +26,17 @@ class AddBusinessCardActivity : AppCompatActivity() {
             finish()
         }
         binding.btnConfirm.setOnClickListener {
-
+            val businessCard = BusinessCard(
+                name = binding.tilName.editText?.text.toString(),
+                business = binding.tilBusiness.editText?.text.toString(),
+                phone = binding.tilPhone.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                customBackground = binding.tilColor.editText?.text.toString()
+            )
+            mainViewModel.insert(businessCard)
+            Toast.makeText(this, R.string.label_show_success, Toast.LENGTH_SHORT).show()
+            finish()
         }
+
     }
 }
