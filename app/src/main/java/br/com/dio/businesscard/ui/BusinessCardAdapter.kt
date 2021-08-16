279ddba42e1f74e6e39e3241c4ae051b2fd20da9
Vitor Ferreira <vitorfer238@gmail.com>
2021-08-16 19:17:53 -0300

    new business card design

--- app/src/main/java/br/com/dio/businesscard/ui/BusinessCardAdapter.kt
+++ app/src/main/java/br/com/dio/businesscard/ui/BusinessCardAdapter.kt
@@ -1,14 +1,19 @@
 package br.com.dio.businesscard.ui
 
 import android.graphics.Color
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import br.com.dio.businesscard.R
 import br.com.dio.businesscard.data.BusinessCard
 import br.com.dio.businesscard.databinding.ItemBusinessCardBinding
+
 
 class BusinessCardAdapter :
     ListAdapter<BusinessCard, BusinessCardAdapter.ViewHolder>(DiffCallback()) {
@@ -33,7 +38,19 @@ class BusinessCardAdapter :
             binding.tvPhone.text = item.phone
             binding.tvEmail.text = item.email
             binding.tvBusinessName.text = item.business
-            binding.mcvContent.setCardBackgroundColor(Color.parseColor(item.customBackground))
+
+            try {
+                val unwrappedDrawable = AppCompatResources.getDrawable(
+                    binding.ivCardIllustration.context,
+                    R.drawable.card_illustration
+                )
+                val wrappedDrawable = DrawableCompat.wrap(unwrappedDrawable!!)
+                DrawableCompat.setTint(wrappedDrawable, Color.parseColor(item.customBackground))
+                unwrappedDrawable.mutate()
+                binding.ivCardIllustration.setImageDrawable(unwrappedDrawable)
+            } catch (e: Exception) {
+                Log.e(BusinessCardAdapter::class.simpleName, "failed to color drawable")
+            }
             binding.mcvContent.setOnClickListener {
                 listenerShare(it)
             }
